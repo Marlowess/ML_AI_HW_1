340df42e2cf5508238c7278a85d759fa454d7ed4
Stefano Brilli <s249914@studenti.polito.it>
2018-10-27 10:08:06 +0200

    Rappresentazione grafica completata

--- run.py
+++ run.py
@@ -1,24 +1,35 @@
 from PIL import Image
 import numpy as np
 import os
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 # This method opens each class folder and gets raw pixels of each image
-def getData(directory_name, x):
+def getData(directory_name, x, y, count, label):
     directory = os.fsencode(directory_name)
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         i = Image.open(directory_name + filename)
         x.append(np.asarray(i).ravel())
+        y.insert(count, label)
+        count += 1
 
 
 x = [] # list of items
-rootFolder = 'PACS_homework/' # root images folder
+y = []
+count = 0
+rootFolder = '/home/stefano/Documenti/Politecnico/Magistrale/2 Anno/ML/Homework/#1/PACS_homework/' # root images folder
 folder1 = 'dog/'
 folder2 = 'guitar/'
 folder3 = 'house/'
 folder4 = 'person/'
 
-getData(rootFolder + folder1, x)
-getData(rootFolder + folder2, x)
-getData(rootFolder + folder3, x)
-getData(rootFolder + folder4, x)
+getData(rootFolder + folder1, x, y, count, 1)
+getData(rootFolder + folder2, x, y, count, 2)
+getData(rootFolder + folder3, x, y, count, 3)
+getData(rootFolder + folder4, x, y, count, 4)
+
+x = np.asarray(x)
+
+X_t = PCA(2).fit_transform(x)
+plt.scatter(X_t[:,0], X_t[:,1], c=y)
