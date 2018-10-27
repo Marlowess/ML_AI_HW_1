b7d0983e29a0d16b589f28f5d77e09558bd6d4cb
Stefano Brilli <s249914@studenti.polito.it>
2018-10-27 18:43:24 +0200

    Iniziata analisi PCA su singola immagine

--- run.py
+++ run.py
@@ -4,53 +4,61 @@ import os
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import matplotlib.colors as colo
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
+# vector representing how many elements there are in each folder
+# 0:dog 1:guitar 2:house 3:person
 numbers = [0, 0, 0, 0]
+y = []
+count = 0
 
 # This method opens each class folder and gets raw pixels of each image
-def getData(directory_name, x, y, count, label):
+def getData(directory_name, x, label):
     directory = os.fsencode(directory_name)
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         i = Image.open(directory_name + filename)
         x.append(np.asarray(i).ravel())
+        global y
+        global count
         y.insert(count, label)
         count += 1
         global numbers
         numbers[label]  = numbers[label] + 1
 
-def normalize(matrix):
-    matrix = (matrix - np.mean(matrix, axis=0, dtype=np.float64))/(np.std(matrix, axis=0,dtype=np.float64))
-    return matrix
 
-
+# Step 1
 x = [] # list of items
-y = []
-count = 0
 rootFolder = '/home/stefano/Documenti/Politecnico/Magistrale/2 Anno/ML/Homework/#1/PACS_homework/' # root images folder
 folder1 = 'dog/'
 folder2 = 'guitar/'
 folder3 = 'house/'
 folder4 = 'person/'
 
-getData(rootFolder+folder1, x, y, count, 0)
-getData(rootFolder+folder2, x, y, count, 1)
-getData(rootFolder+folder3, x, y, count, 2)
-getData(rootFolder+folder4, x, y, count, 3)
-
-#print(numbers)
+getData(rootFolder+folder1, x, 0)
+getData(rootFolder+folder2, x, 1)
+getData(rootFolder+folder3, x, 2)
+getData(rootFolder+folder4, x, 3)
 
 
+
+# Step 2
+img = Image.open(rootFolder+folder1+'056_0026.jpg')
+im_grey = img.convert('L')
+im_array = np.array(im_grey)
+#print(im_array.shape)
+img_pca = PCA(60).fit_transform(im_array)
+print(img_pca.shape)
+Image.fromarray(img_pca).show()
+
+
+# Step 3
 x = np.asarray(x, dtype=np.float64)
-x = (x - np.mean(x, axis=0, dtype=np.float64))/(np.std(x, axis=0,dtype=np.float64))
-cov_x = np.cov(x)
+x = StandardScaler().fit_transform(x)
 np.set_printoptions(threshold=np.nan)
-#print(cov_x)
-#y = np.asarray(y).ravel()
+X_t = PCA(60).fit_transform(x)
 
-X_t = PCA(2).fit_transform(cov_x)
-
-#plt.scatter(X_t[:,0], X_t[:,1], c=y, cmap=colo.ListedColormap(colors))
 dogIndex = numbers[0]-1
 guitarIndex = numbers[0]+numbers[1]-1
 houseIndex = numbers[0]+numbers[1]+numbers[2]-1
